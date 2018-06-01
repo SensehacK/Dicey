921cb7d9a5dd6479788371f07843b4b11ccbea63
Kautilya <kautilyasave@gmail.com>
2018-06-01 10:09:29 +0530

    Added reusable component

--- Dicey/ViewController.swift
+++ Dicey/ViewController.swift
@@ -43,7 +43,22 @@ class ViewController: UIViewController {
 
     
     @IBAction func rollDicePressed(_ sender: Any) {
-        
+        print("rollDicePressed")
+        // call function
+        randomDices()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            
+        }
+        else {
+            print("No shakes for you today")
+        }
+    }
+    
+    // Random Logic for Dice
+    func randomDices() {
         //Getting random Integer values
         let randomInt1 : Int = Int(arc4random_uniform(6))
         let randomInt2 : Int = Int(arc4random_uniform(6))
@@ -51,22 +66,11 @@ class ViewController: UIViewController {
         // Debug prints Random Dices
         print(diceStr + String(randomInt1))
         print(diceStr + String(randomInt2))
-
+        
         // Setting images for random dice.
         diceImage1.image = UIImage(named: diceStr + String(randomInt1 + 1))
         diceImage2.image = UIImage(named: diceStr + String(randomInt2 + 1))
-        
     }
-    
-    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
-        if motion == .motionShake {
-            diceImage1.image = #imageLiteral(resourceName: "dice6")
-        }
-        else {
-            print("No shakes for you today")
-        }
-    }
-    
 
 }
 
